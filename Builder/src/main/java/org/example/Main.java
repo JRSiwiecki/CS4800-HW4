f01fceeb6074aa1f04664b4fede2ab71cca20a48
JRSiwiecki <JRSiwiecki@gmail.com>
2024-03-11 20:17:42 -0700

    Add last pizzas to driver

--- Builder/src/main/java/org/example/Main.java
+++ Builder/src/main/java/org/example/Main.java
@@ -59,5 +59,49 @@ public class Main
                 .bakePizza();
         pizza5.eat();
 
+        // Next two Little Caesars pizzas
+        Pizza pizza6 = new PizzaBuilder()
+                .setChain(Pizza.PizzaChain.LITTLE_CAESARS)
+                .setSize(Pizza.PizzaSize.MEDIUM)
+                .setPepperoni(true)
+                .setSausage(true)
+                .setMushrooms(true)
+                .setBacon(true)
+                .setOnions(true)
+                .setExtraCheese(true)
+                .setPeppers(true)
+                .setChicken(true)
+                .bakePizza();
+        pizza6.eat();
+
+        Pizza pizza7 = new PizzaBuilder()
+                .setChain(Pizza.PizzaChain.LITTLE_CAESARS)
+                .setSize(Pizza.PizzaSize.SMALL)
+                .setHamAndPineapple(true)
+                .setSpicyPork(true)
+                .setPesto(true)
+                .setHam(true)
+                .setBeef(true)
+                .setTomatoAndBasil(true)
+                .bakePizza();
+        pizza7.eat();
+
+        // Next two Dominos pizzas
+        Pizza pizza8 = new PizzaBuilder()
+                .setChain(Pizza.PizzaChain.DOMINOES)
+                .setSize(Pizza.PizzaSize.SMALL)
+                .setPepperoni(true)
+                .bakePizza();
+        pizza8.eat();
+
+        Pizza pizza9 = new PizzaBuilder()
+                .setChain(Pizza.PizzaChain.DOMINOES)
+                .setSize(Pizza.PizzaSize.LARGE)
+                .setPepperoni(true)
+                .setPeppers(true)
+                .setHam(true)
+                .bakePizza();
+        pizza9.eat();
+
     }
 }
